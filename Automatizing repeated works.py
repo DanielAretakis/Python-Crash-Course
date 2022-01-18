1e59e5cfd0d11f3a8c1a1be91e1b5590248413a6
DanielAretakis <daniel.aretakis@hotmail.com>
2022-01-18 16:01:51 -0300

    Class 1

---
+++ Automatizing repeated works.py
@@ -0,0 +1,43 @@
+import time
+
+import pandas as pd
+import pyautogui
+import pyperclip
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.press('win')
+pyautogui.write('Opera')
+pyautogui.press('enter')
+pyautogui.press('enter')
+pyperclip.copy('https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
+time.sleep(3)
+pyautogui.doubleClick(x=384, y=286)
+time.sleep(2)
+pyautogui.rightClick(x=384, y=286)
+pyautogui.click(x=454, y=725)
+time.sleep(5)
+pyautogui.press('enter')
+tabela = pd.read_excel(r'D:\Programação\Intensivo Python\Vendas - Dez.xlsx')
+faturamento = tabela['Valor Final'].sum()
+qtdprod = tabela['Quantidade'].sum()
+pyautogui.hotkey('ctrl', 't')
+pyautogui.write('https://outlook.live.com/mail/0/')
+pyautogui.press('enter')
+time.sleep(3)
+pyautogui.click(x=166, y=177)
+time.sleep(2)
+pyautogui.write('daniel.aretakis@hotmail.com')
+pyautogui.press('tab')
+pyperclip.copy('Intensivo Python')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('tab')
+pyperclip.copy('''
+{}
+
+quantidade total de produtos {:,}
+e o faturamento R$ {:,.2f}'''.format(tabela, qtdprod, faturamento))
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.hotkey('ctrl', 'enter')
